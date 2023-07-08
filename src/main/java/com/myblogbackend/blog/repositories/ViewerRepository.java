cd3c9caff29393508db2797a30d3367f1e1b47d9
tuannguyen <tuanquangnguyen1710@gmail.com>
2023-07-08 16:06:00 +0300

    Updated code.

--- src/main/java/com/myblogbackend/blog/repositories/ViewerRepository.java
+++ src/main/java/com/myblogbackend/blog/repositories/ViewerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ViewerRepository extends JpaRepository<ViewersEntity, UUID> {
@@ -14,5 +15,7 @@ public interface ViewerRepository extends JpaRepository<ViewersEntity, UUID> {
     @Modifying
     @Query("UPDATE ViewersEntity SET viewCounter = viewCounter + 1 where postId = :postId")
     void updateViewer(@Param("postId") UUID postId);
+
+    Optional<ViewersEntity> findByPostId(UUID postId);
 
 }
--- src/main/java/com/myblogbackend/blog/services/impl/ViewerServiceImpl.java
+++ src/main/java/com/myblogbackend/blog/services/impl/ViewerServiceImpl.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -29,16 +30,22 @@ public class ViewerServiceImpl implements ViewerService {
         var post = postRepository
                 .findById(postId)
                 .orElseThrow(() -> new BlogRuntimeException(ErrorCode.ID_NOT_FOUND));
-        if (viewerRepository.existsByPostId(post.getId())) {
-            logger.error("Update viewer successfully by id {} ", post.getId());
-            viewerRepository.updateViewer(post.getId());
-            return;
+
+        ViewersEntity viewers = viewerRepository.findByPostId(postId)
+                .orElseGet(() -> ViewersEntity.builder()
+                        .viewCounter(1)
+                        .postId(postId)
+                        .build());
+
+        viewers.setViewCounter(viewers.getViewCounter() + 1);
+
+        try {
+            viewerRepository.save(viewers);
+            logger.error("Update viewer successfully for postId: {}", postId);
+        } catch (ObjectOptimisticLockingFailureException ex) {
+            // If another request has modified the entity, retry the update
+            logger.warn("Optimistic locking failure for postId: {}", postId);
+            countUserViewer(postId); // Retry the update
         }
-        var viewers = ViewersEntity.builder()
-                .viewCounter(1)
-                .postId(post.getId())
-                .build();
-        logger.error("Create new viewer successfully for postId by id {} ", post.getId());
-        viewerRepository.save(viewers);
     }
 }
