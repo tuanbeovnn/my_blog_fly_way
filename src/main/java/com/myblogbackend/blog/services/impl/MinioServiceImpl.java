31ba258d2f46d21e51cd85a01d201dcf9f1ee775
Tuan Nguyen <tuan@pezzie.com>
2023-12-31 13:53:39 +0200

    Fixed error 401 and refactor minio configuration

--- src/main/java/com/myblogbackend/blog/services/impl/MinioServiceImpl.java
+++ src/main/java/com/myblogbackend/blog/services/impl/MinioServiceImpl.java
@@ -1,87 +1,113 @@
 package com.myblogbackend.blog.services.impl;
 
-import com.myblogbackend.blog.config.minio.FileMetadata;
-import com.myblogbackend.blog.config.minio.MinioProperties;
+
+import com.myblogbackend.blog.config.minio.MinioConfig;
+import com.myblogbackend.blog.exception.MinioOperationException;
+import com.myblogbackend.blog.response.FileResponse;
 import com.myblogbackend.blog.services.MinioService;
-import io.minio.MinioClient;
-import io.minio.UploadObjectArgs;
-import io.minio.errors.*;
+import com.myblogbackend.blog.utils.MinioUtil;
 import lombok.RequiredArgsConstructor;
-import org.apache.tika.detect.DefaultDetector;
-import org.apache.tika.detect.Detector;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.mime.MediaType;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class MinioServiceImpl implements MinioService {
-    private final MinioClient minioClient;
-    private final MinioProperties minioProperties;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MinioServiceImpl.class);
+    private final MinioUtil minioUtil;
+    private final MinioConfig minioConfig;
+
+    @SneakyThrows
+    @Override
+    public FileResponse putObject(final MultipartFile multipartFile,
+                                  final String bucket, final String fileType) {
+        LOGGER.info("MinioServiceImpl | putObject is called");
+        try {
+            var bucketName = StringUtils.defaultIfBlank(bucket, minioConfig.getBucketName());
+            createBucketIfNotExists(bucketName);
+            String fileName = multipartFile.getOriginalFilename();
+            String objectName = generateObjectName(fileName);
+            LocalDateTime createdTime = LocalDateTime.now();
+            minioUtil.putObject(bucketName, multipartFile, objectName, fileType);
+            LOGGER.info("MinioServiceImpl | putObject | url : " + buildObjectUrl(bucketName, objectName));
+            return buildFileResponse(bucketName, objectName, multipartFile.getSize(), fileType, createdTime);
+        } catch (Exception e) {
+            LOGGER.error("MinioServiceImpl | putObject | Exception : " + e.getMessage());
+            throw new MinioOperationException("Error putting object to Minio", e);
+        }
+    }
+
+    @SneakyThrows
+    @Override
+    public List<FileResponse> putObjects(final MultipartFile[] multipartFiles,
+                                         final String bucket, final String fileType) {
+        var bucketName = StringUtils.defaultIfBlank(bucket, minioConfig.getBucketName());
+        createBucketIfNotExists(bucketName);
+        List<FileResponse> files = new ArrayList<>();
+        for (MultipartFile file : multipartFiles) {
+            FileResponse fileResponse = processFile(file, bucketName, fileType);
+            files.add(fileResponse);
+        }
+
+        return files;
+    }
+
+    private FileResponse processFile(final MultipartFile file, final
+    String bucketName, final String fileType) {
+        String fileName = file.getOriginalFilename();
+        String objectName = generateObjectName(fileName);
+        LocalDateTime createdTime = LocalDateTime.now();
+
+        minioUtil.putObject(bucketName, file, objectName, fileType);
+
+        return buildFileResponse(bucketName, objectName, file.getSize(), fileType, createdTime);
+    }
+
+    private void createBucketIfNotExists(final String bucketName) {
+        if (!bucketExists(bucketName)) {
+            makeBucket(bucketName);
+            LOGGER.info("MinioServiceImpl | createBucketIfNotExists | bucketName : " + bucketName + " created");
+        }
+    }
+
+    private String generateObjectName(final String fileName) {
+        return UUID.randomUUID().toString().replaceAll("-", "")
+                + StringUtils.defaultString(fileName).substring(StringUtils.defaultString(fileName).lastIndexOf("."));
+    }
+
+    private FileResponse buildFileResponse(final String bucketName, final String objectName, final long fileSize,
+                                           final String fileType, final LocalDateTime createdTime) {
+        return FileResponse.builder()
+                .filename(buildObjectUrl(bucketName, objectName))
+                .fileSize(fileSize)
+                .contentType(fileType)
+                .createdTime(createdTime)
+                .build();
+    }
+
+    private String buildObjectUrl(final String bucketName, final String objectName) {
+        return minioConfig.getEndpoint() + "/" + bucketName + "/" + objectName;
+    }
 
     @Override
-    public FileMetadata uploadFile(final MultipartFile multipartFile) {
-        //check if mimeType includes (image) -> throw exception
-        String fileKey = createKey(multipartFile);
-        return put(minioProperties.getBucketName(), fileKey, multipartFile, true);
+    public boolean bucketExists(final String bucketName) {
+        LOGGER.info("MinioServiceImpl | bucketExists is called");
+        return minioUtil.bucketExists(bucketName);
     }
 
-    private String createKey(final MultipartFile file) {
-        String tagFile = file.getOriginalFilename();
-        if (tagFile == null) {
-            throw new RuntimeException("File is empty");
-        }
-        String splitTag = tagFile.substring(tagFile.lastIndexOf("."));
-        int tagFileSub = tagFile.lastIndexOf(".");
-        String name = tagFile.substring(0, Math.min(tagFileSub, 10));
-        return name + UUID.randomUUID() + splitTag;
-    }
-
-    public FileMetadata put(final String bucket, final String key, final MultipartFile file, final Boolean publicAccess) {
-        var metadata = FileMetadata.builder()
-                .bucket(bucket)
-                .key(key)
-                .name(file.getOriginalFilename())
-                .extension(StringUtils.getFilenameExtension(file.getOriginalFilename()))
-                .mime(detectMimeType(file))
-                .size(file.getSize())
-                .build();
-
-
-        try {
-            var upload = UploadObjectArgs
-                    .builder()
-                    .bucket(minioProperties.getBucketName())
-                    .filename(metadata.getKey())
-                    .contentType(metadata.getMime())
-                    .build();
-            minioClient.uploadObject(upload);
-        } catch (IOException e) {
-            throw new RuntimeException("IOException");
-        } catch (ServerException | InsufficientDataException | ErrorResponseException | NoSuchAlgorithmException |
-                 InvalidKeyException | InvalidResponseException | XmlParserException | InternalException e) {
-            throw new RuntimeException(e);
-        }
-        return metadata;
-    }
-
-    public String detectMimeType(final MultipartFile multipartFile) {
-        Detector detector = new DefaultDetector();
-        Metadata metadata = new Metadata();
-        try (InputStream stream = multipartFile.getInputStream()) {
-            MediaType mediaType = detector.detect(stream, metadata);
-            return mediaType.toString();
-        } catch (IOException e) {
-            return "";
-        }
+    @Override
+    public void makeBucket(final String bucketName) {
+        LOGGER.info("MinioServiceImpl | makeBucket is called");
+        LOGGER.info("MinioServiceImpl | makeBucket | bucketName : " + bucketName);
+        minioUtil.makeBucket(bucketName);
     }
 }
