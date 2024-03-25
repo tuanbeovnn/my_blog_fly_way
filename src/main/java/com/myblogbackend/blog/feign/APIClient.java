969bfea56d04a223c91b8002084f382ed44e6f15
Tuan Nguyen <tuan@pezzie.com>
2024-03-25 21:56:48 +0200

    Updated properties file.

--- src/main/java/com/myblogbackend/blog/feign/APIClient.java
+++ src/main/java/com/myblogbackend/blog/feign/APIClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@FeignClient(value = "uploadFiles", url = "http://localhost:8081", configuration = FeignSupportConfig.class)
+@FeignClient(value = "uploadFiles", url = "${file.upload.url}", configuration = FeignSupportConfig.class)
 public interface APIClient {
     @PostMapping(value = "/minio/upload/files", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     List<FileResponse> uploadMultipleFiles(final MultipartFile[] files);
