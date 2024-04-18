d9586f164dff221fbb475dc959ffd214a2a8edfc
Tuan Nguyen <tuan@pezzie.com>
2024-04-18 21:17:48 +0300

    Updated cors.

--- src/main/java/com/myblogbackend/blog/BlogApplication.java
+++ src/main/java/com/myblogbackend/blog/BlogApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
@@ -18,16 +15,5 @@ public class BlogApplication {
     public static void main(final String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(final CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
-    }
-
 
 }
