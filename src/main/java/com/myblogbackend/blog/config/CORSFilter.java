5fd082cfed7e23a8723fb417250d5ea838919e75
Tuan Nguyen <tuan@pezzie.com>
2024-06-01 16:19:02 +0300

    Remove cors filter

--- src/main/java/com/myblogbackend/blog/config/CORSFilter.java
+++ src/main/java/com/myblogbackend/blog/config/CORSFilter.java
@@ -1,43 +1,43 @@
-package com.myblogbackend.blog.config;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CORSFilter implements Filter {
-
-    public CORSFilter() {
-    }
-
-    public void doFilter(final ServletRequest req,
-                         final ServletResponse res,
-                         final FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, x-auth-token, origin, content-type, accept");
-
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            chain.doFilter(req, res);
-        }
-    }
-
-    @Override
-    public void init(final FilterConfig filterConfig) {
-    }
-
-    @Override
-    public void destroy() {
-    }
-}
+//package com.myblogbackend.blog.config;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.core.Ordered;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//public class CORSFilter implements Filter {
+//
+//    public CORSFilter() {
+//    }
+//
+//    public void doFilter(final ServletRequest req,
+//                         final ServletResponse res,
+//                         final FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) res;
+//        HttpServletRequest request = (HttpServletRequest) req;
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, x-auth-token, origin, content-type, accept");
+//
+//        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+//            response.setStatus(HttpServletResponse.SC_OK);
+//        } else {
+//            chain.doFilter(req, res);
+//        }
+//    }
+//
+//    @Override
+//    public void init(final FilterConfig filterConfig) {
+//    }
+//
+//    @Override
+//    public void destroy() {
+//    }
+//}
