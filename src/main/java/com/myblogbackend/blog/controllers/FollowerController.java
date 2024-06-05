5b5f15e0ea4c396b5ded179e766750f9b1fe8431
Tuan Nguyen <tuan.nguyen@svea.fi>
2024-06-05 14:00:03 +0300

    User following updated code.

--- src/main/java/com/myblogbackend/blog/controllers/FollowerController.java
+++ src/main/java/com/myblogbackend/blog/controllers/FollowerController.java
@@ -2,7 +2,7 @@ package com.myblogbackend.blog.controllers;
 
 
 import com.myblogbackend.blog.controllers.route.CommonRoutes;
-import com.myblogbackend.blog.controllers.route.FavoriteRoutes;
+import com.myblogbackend.blog.controllers.route.FollowerRoutes;
 import com.myblogbackend.blog.response.ResponseEntityBuilder;
 import com.myblogbackend.blog.services.FollowerService;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +16,13 @@ import java.util.UUID;
 
 
 @RestController
-@RequestMapping(CommonRoutes.BASE_API + CommonRoutes.VERSION + FavoriteRoutes.BASE_URL)
+@RequestMapping(CommonRoutes.BASE_API + CommonRoutes.VERSION + FollowerRoutes.BASE_URL)
 @RequiredArgsConstructor
 public class FollowerController {
     private final FollowerService followerService;
 
     @PutMapping("/{userId}")
-    public ResponseEntity<?> followingUser(@PathVariable final UUID userId) {
+    public ResponseEntity<?> followingUser(@PathVariable(value = "userId") final UUID userId) {
         followerService.persistOrDelete(userId);
         return ResponseEntityBuilder
                 .getBuilder()
