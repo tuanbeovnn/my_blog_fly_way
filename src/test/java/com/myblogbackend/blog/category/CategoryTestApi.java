3de4bc5a7141944fb1af575be81003597365fcb7
tuannguyen <tuanquangnguyen1710@gmail.com>
2024-06-15 01:31:31 +0300

    remove method

--- src/test/java/com/myblogbackend/blog/category/CategoryTestApi.java
+++ src/test/java/com/myblogbackend/blog/category/CategoryTestApi.java
@@ -2,7 +2,6 @@ package com.myblogbackend.blog.category;
 
 import com.myblogbackend.blog.models.CategoryEntity;
 import com.myblogbackend.blog.request.CategoryRequest;
-import com.myblogbackend.blog.response.CategoryResponse;
 
 import java.util.UUID;
 
@@ -18,19 +17,6 @@ public final class CategoryTestApi {
     public static CategoryRequest prepareCategoryForRequesting() {
         return CategoryRequest.builder()
                 .name("Category A")
-                .build();
-    }
-
-    public static CategoryResponse toCategoryResponse(final CategoryEntity categoryEntity) {
-        return CategoryResponse.builder()
-                .id(categoryEntity.getId())
-                .name(categoryEntity.getName())
-                .build();
-    }
-
-    public static CategoryRequest prepareCategoryForRequestingUpdate(final String name) {
-        return CategoryRequest.builder()
-                .name(name)
                 .build();
     }
 
