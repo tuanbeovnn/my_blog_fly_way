e7a84f786af21e0433f58b8310b50e0ffddc7bd3
tuannguyen <tuanquangnguyen1710@gmail.com>
2024-06-26 16:30:23 -0500

    update response comment

--- src/main/java/com/myblogbackend/blog/controllers/CommentController.java
+++ src/main/java/com/myblogbackend/blog/controllers/CommentController.java
@@ -6,12 +6,18 @@ import com.myblogbackend.blog.request.CommentRequest;
 import com.myblogbackend.blog.services.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping(CommonRoutes.BASE_API + CommonRoutes.VERSION + CommentRoutes.BASE_URL)
+@RequestMapping(CommonRoutes.BASE_API + CommonRoutes.VERSION)
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
@@ -22,7 +28,7 @@ public class CommentController {
         return ResponseEntity.ok(commentResponse);
     }
 
-    @GetMapping("/{postId}")
+    @GetMapping("/public" + CommentRoutes.BASE_URL + "/{postId}")
     public ResponseEntity<?> getListCommentsByPostId(
             @RequestParam(name = "offset", defaultValue = "0") final Integer offset,
             @RequestParam(name = "limit", defaultValue = "10") final Integer limit,
