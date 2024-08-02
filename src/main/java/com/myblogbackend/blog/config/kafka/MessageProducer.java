3204064db1722d523d54b6bfe90ea341e2368876
Tuan Nguyen <tuan@pezzie.com>
2024-08-02 23:53:13 +0300

    Update url for kafka.

---
+++ src/main/java/com/myblogbackend/blog/config/kafka/MessageProducer.java
@@ -0,0 +1,19 @@
+package com.myblogbackend.blog.config.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class MessageProducer {
+
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+
+}
