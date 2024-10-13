d57d30e1cad7b68491df40cd8d863de25f38eefc
Tuan Nguyen <tuan@pezzie.com>
2024-10-13 22:31:35 +0300

    Remove email classes.

--- src/main/java/com/myblogbackend/blog/services/EmailSendingService.java
+++
@@ -1,9 +0,0 @@
-package com.myblogbackend.blog.services;
-
-
-import com.myblogbackend.blog.models.UserEntity;
-
-public interface EmailSendingService {
-
-    void sendActivationEmail(UserEntity user);
-}
