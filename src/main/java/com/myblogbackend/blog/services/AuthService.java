19e62cfa9d6d4bc39ea1f302bceae6409ac639c3
Tuan Nguyen <tuan@pezzie.com>
2024-10-13 21:44:24 +0300

    Updated run check style.

--- src/main/java/com/myblogbackend/blog/services/AuthService.java
+++ src/main/java/com/myblogbackend/blog/services/AuthService.java
@@ -1,12 +1,6 @@
 package com.myblogbackend.blog.services;
 
-import com.myblogbackend.blog.enums.NotificationType;
-import com.myblogbackend.blog.models.UserEntity;
-import com.myblogbackend.blog.request.ForgotPasswordRequest;
-import com.myblogbackend.blog.request.LoginFormOutboundRequest;
-import com.myblogbackend.blog.request.LoginFormRequest;
-import com.myblogbackend.blog.request.SignUpFormRequest;
-import com.myblogbackend.blog.request.TokenRefreshRequest;
+import com.myblogbackend.blog.request.*;
 import com.myblogbackend.blog.response.JwtResponse;
 import com.myblogbackend.blog.response.UserResponse;
 import freemarker.template.TemplateException;
@@ -16,8 +10,6 @@ import java.io.IOException;
 
 public interface AuthService {
     JwtResponse userLogin(LoginFormRequest loginFormRequest);
-
-//    UserResponse registerUser(SignUpFormRequest signUpRequest) throws TemplateException, IOException;
 
     UserResponse registerUserV2(SignUpFormRequest signUpRequest) throws TemplateException, IOException;
 
