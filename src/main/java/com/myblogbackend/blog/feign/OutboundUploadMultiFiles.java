b381de2b816cffb70c0c8d4759602074bbb51125
Tuan Nguyen <tuan@pezzie.com>
2025-03-02 16:07:04 +0200

    Updated application properties files.

--- src/main/java/com/myblogbackend/blog/feign/OutboundUploadMultiFiles.java
+++ src/main/java/com/myblogbackend/blog/feign/OutboundUploadMultiFiles.java
@@ -1,6 +1,7 @@
 package com.myblogbackend.blog.feign;
 
 import com.myblogbackend.blog.feign.configs.FeignFallbackHandler;
+import com.myblogbackend.blog.feign.configs.UploadFilesFeignWithBasicAuthenticationConfig;
 import com.myblogbackend.blog.response.FileResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -14,14 +15,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@FeignClient(name = "people-api", url = "${file.upload.url}")
+@FeignClient(name = "upload-minio-api", url = "${file.upload.url}", configuration = UploadFilesFeignWithBasicAuthenticationConfig.class)
 public interface OutboundUploadMultiFiles {
 
     Logger logger = LogManager.getLogger(OutboundUploadMultiFiles.class);
 
     @PostMapping(value = "/minio/upload/files", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @Retry(name = "peopleProxyRetry")
-    @CircuitBreaker(name = "peopleProxyCircuitBreaker", fallbackMethod = "serviceFallbackMethod")
+    @Retry(name = "uploadMinioProxyRetry")
+    @CircuitBreaker(name = "uploadMinioProxyCircuitBreaker", fallbackMethod = "serviceFallbackMethod")
     List<FileResponse> uploadMultipleFiles(@RequestPart("files") final MultipartFile[] files);
 
     default List<FileResponse> serviceFallbackMethod(final Throwable exception) {
