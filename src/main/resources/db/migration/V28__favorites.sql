0dc418b18886597b09826df5204482b2fd1acf43
thoanguyen2907 <thoanguyen290795@gmail.com>
2025-08-17 14:11:06 +0300

    combine sql create table and update in one file

--- src/main/resources/db/migration/V28__favorites.sql
+++ src/main/resources/db/migration/V28__favorites.sql
@@ -1,4 +1,4 @@
-create table favorites(
+create table if not exists blog_dbo2.favorites(
     id UUID primary key default gen_random_uuid(),
     user_id UUID not null unique ,
     post_id UUID,
@@ -14,3 +14,13 @@ create table favorites(
     foreign key (post_id) references posts(id),
     foreign key (comment_id) references comments(id)
 );
+
+ALTER TABLE blog_dbo2.comments
+    ADD COLUMN IF NOT EXISTS likes BIGINT;
+
+UPDATE blog_dbo2.comments
+SET likes = 0 where likes is null;
+
+ALTER TABLE blog_dbo2.comments
+    ALTER COLUMN likes SET DEFAULT 0,
+ALTER COLUMN likes SET NOT NULL;
--- src/main/resources/db/migration/V29__Update_Comment_Like.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE blog_dbo2.comments
-ADD COLUMN IF NOT EXISTS likes BIGINT;
-
-UPDATE blog_dbo2.comments
-SET likes = 0 where likes is null;
-
-ALTER TABLE blog_dbo2.comments
-ALTER COLUMN likes SET DEFAULT 0,
-ALTER COLUMN likes SET NOT NULL;
